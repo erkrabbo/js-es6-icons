018871ff52c3b1be3e71e84395a38c464255027c
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-09 16:15:16 +0100

    forEach

--- data.js
+++ data.js
@@ -119,8 +119,11 @@ const filter = document.body.querySelector('#js-select');
 const animalList = [];
 const vegetableList = [];
 const userlList = [];
+const all = [];
 
 icons.map(element =>{
+	all.push(element);
+
 	switch (element.type){
 		case 'animal':
 			animalList.push(element);
@@ -133,29 +136,23 @@ icons.map(element =>{
 	}
 });
 
-// const animalList = icons.filter((element) =>{
-// 	return element.type == 'animal';
-// });
-// const vegetableList = icons.filter((element) =>{
-// 	return element.type == 'vegetable';
-// });
-// const userlList = icons.filter((element) =>{
-// 	return element.type == 'user';
-// });
+const lists = [all, animalList, vegetableList, userlList];
 
-// filter.addEventListener('load', generatePanlesByFilter);
 window.addEventListener('load', generatePanlesByFilter);
 filter.addEventListener('change', generatePanlesByFilter);
 
 
 
+
+
 function generatePanlesByFilter(){
-	iconsContainer.innerHTML = '';
-	const choice = filter.value;
+	const choice = parseInt(filter.value);
 
-	switch (choice){
-		case 'animal':
-			animalList.forEach(element => {
+	lists.forEach((element, index) => {
+		if (choice == index){
+			iconsContainer.innerHTML = '';
+
+			element.forEach(element => {
 				const panel = document.createElement('div');
 			
 				panel.classList.add('main-element');
@@ -174,93 +171,7 @@ function generatePanlesByFilter(){
 				icon.style.color = `${element.color}`
 			
 				iconsContainer.appendChild(panel);
-			});
-			break;
-		case 'user':
-			userlList.forEach(element => {
-				const panel = document.createElement('div');
-			
-				panel.classList.add('main-element');
-				panel.style.display = 'flex';
-				panel.style.flexDirection = 'column';
-				panel.style.textAlign = 'center';
-				panel.style.backgroundColor = 'lightgrey';
-				panel.style.borderRadius = '1rem';
-			
-				panel.innerHTML = `
-				<i class="${element.family} ${element.prefix}${element.name}"></i>
-				<span>${element.name}</span>
-				`;
-			
-				const icon = panel.querySelector('i');
-				icon.style.color = `${element.color}`
-			
-				iconsContainer.appendChild(panel);
-			});
-			break;
-		case 'vegetable':
-			vegetableList.forEach(element => {
-				const panel = document.createElement('div');
-			
-				panel.classList.add('main-element');
-				panel.style.display = 'flex';
-				panel.style.flexDirection = 'column';
-				panel.style.textAlign = 'center';
-				panel.style.backgroundColor = 'lightgrey';
-				panel.style.borderRadius = '1rem';
-			
-				panel.innerHTML = `
-				<i class="${element.family} ${element.prefix}${element.name}"></i>
-				<span>${element.name}</span>
-				`;
-			
-				const icon = panel.querySelector('i');
-				icon.style.color = `${element.color}`
-			
-				iconsContainer.appendChild(panel);
-			});
-			break;
-		default:
-			icons.forEach(element => {
-				const panel = document.createElement('div');
-			
-				panel.classList.add('main-element');
-				panel.style.display = 'flex';
-				panel.style.flexDirection = 'column';
-				panel.style.textAlign = 'center';
-				panel.style.backgroundColor = 'lightgrey';
-				panel.style.borderRadius = '1rem';
-			
-				panel.innerHTML = `
-				<i class="${element.family} ${element.prefix}${element.name}"></i>
-				<span>${element.name}</span>
-				`;
-			
-				const icon = panel.querySelector('i');
-				icon.style.color = `${element.color}`
-			
-				iconsContainer.appendChild(panel);
-			});
+				});
+			}
+		})	
 	}
-
-	// icons.forEach(element => {
-	// 	const panel = document.createElement('div');
-	
-	// 	panel.classList.add('main-element');
-	// 	panel.style.display = 'flex';
-	// 	panel.style.flexDirection = 'column';
-	// 	panel.style.textAlign = 'center';
-	// 	panel.style.backgroundColor = 'lightgrey';
-	// 	panel.style.borderRadius = '1rem';
-	
-	// 	panel.innerHTML = `
-	// 	<i class="${element.family} ${element.prefix}${element.name}"></i>
-	// 	<span>${element.name}</span>
-	// 	`;
-	
-	// 	const icon = panel.querySelector('i');
-	// 	icon.style.color = `${element.color}`
-	
-	// 	iconsContainer.appendChild(panel);
-	// });
-}
